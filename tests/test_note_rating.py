90f477f32c0185b7719c3ba82ce2083491841580
kenza39 <scykenza59@gmail.com>
2025-11-12 20:52:52 +0100

    red - test rating 8 returns unsuccessful

--- tests/test_note_rating.py
+++ tests/test_note_rating.py
@@ -4,3 +4,6 @@ def test_rate_9_returns_unsuccessful():
      assert rate_note(9) == "unsuccessful"
 def test_rate_10_returns_acceptable():
     assert rate_note(10) == "acceptable"
+
+def test_rate_8_returns_unsuccessful():
+        assert rate_note(8) == "unsuccessful"
