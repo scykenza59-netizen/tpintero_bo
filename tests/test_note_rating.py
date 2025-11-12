e9f1e3b0f02a535d9c3c2e57495c9697c587baa7
kenza39 <scykenza59@gmail.com>
2025-11-12 20:54:13 +0100

    green - rating 8 returns unsuccessful

--- tests/test_note_rating.py
+++ tests/test_note_rating.py
@@ -1,9 +1,7 @@
 from note_rating import rate_note
-
 def test_rate_9_returns_unsuccessful():
      assert rate_note(9) == "unsuccessful"
 def test_rate_10_returns_acceptable():
     assert rate_note(10) == "acceptable"
-
 def test_rate_8_returns_unsuccessful():
         assert rate_note(8) == "unsuccessful"
