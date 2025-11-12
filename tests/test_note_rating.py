1dea71784e916616d36517e68b89c095451a855d
kenza39 <scykenza59@gmail.com>
2025-11-12 20:57:58 +0100

    red - test rate 7 returns unsuccessful

--- tests/test_note_rating.py
+++ tests/test_note_rating.py
@@ -1,15 +1,6 @@
 import pytest
 from note_rating import rate_note
-def test_rate_9_returns_unsuccessful():
-    assert rate_note(9) == "unsuccessful"
 @pytest.mark.parametrize("note", [8, 9])
+@pytest.mark.parametrize("note", [7, 8, 9])
 def test_rate_note_unsuccessful(note):
     assert rate_note(note) == "unsuccessful"
-
-
-def test_rate_10_returns_acceptable():
-    assert rate_note(10) == "acceptable"
-
-
-def test_rate_8_returns_unsuccessful():
-    assert rate_note(8) == "unsuccessful"
