ba2f83caa81ba13610a584d68f34cec1cef477f9
kenza39 <scykenza59@gmail.com>
2025-11-12 21:59:46 +0100

    blue - refactoring

--- tests/test_note_rating.py
+++ tests/test_note_rating.py
@@ -1,6 +1,17 @@
 import pytest
 from note_rating import rate_note
 
+@pytest.mark.parametrize(
+    "note, expected",
+    [
+        (7, "unsuccessful"),
+        (8, "unsuccessful"),
+        (9, "unsuccessful"),
+        (10, "acceptable"),
+        (11, "acceptable"),
+        (12, "good"),
+    ])
+def test_note_result(note, expected):
+    actual_result = rate_note(note)
+    assert actual_result == expected
 
-def test_rate_12_is_good():
-    assert rate_note(12) == "good"
