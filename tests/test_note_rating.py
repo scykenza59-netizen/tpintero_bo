d7e35faa2bc88eb30ac550be5ff1fdc0a6b7e1a1
kenza39 <scykenza59@gmail.com>
2025-11-12 20:51:50 +0100

    Green - rating 10 returns acceptable

--- tests/test_note_rating.py
+++ tests/test_note_rating.py
@@ -1,8 +1,6 @@
 from note_rating import rate_note
 
-
-
 def test_rate_9_returns_unsuccessful():
-        assert rate_note(9) == "unsuccessful"
+     assert rate_note(9) == "unsuccessful"
 def test_rate_10_returns_acceptable():
     assert rate_note(10) == "acceptable"
