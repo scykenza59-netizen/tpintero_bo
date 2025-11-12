3fc632cb8bab1faa643005aa9acd4da0eac5c78c
kenza39 <scykenza59@gmail.com>
2025-11-12 21:44:21 +0100

    green-rate note 12 returns good

--- tests/test_note_rating.py
+++ tests/test_note_rating.py
@@ -1,8 +1,6 @@
 import pytest
 from note_rating import rate_note
 
-@pytest.mark.parametrize("note", [7, 8, 9])
-def test_rate_note_unsuccessful(note):
-    assert rate_note(note) == "unsuccessful"
+
 def test_rate_12_is_good():
     assert rate_note(12) == "good"
